470ca1f783f3d73c83ee327bbd78efe9683c6108
cpheighter <cheighterjr@gmail.com>
2022-07-07 12:20:49 -0400

    init

---
+++ GuessingGame/Program.cs
@@ -0,0 +1,26 @@
+﻿var favNum = 0;
+
+do
+{
+    Console.Write("Guess my favorite number \nYour guess: ");
+    var guessNum = int.Parse(Console.ReadLine());
+
+    if (guessNum < 0)
+    {
+        Console.WriteLine("You're guess is too low. Try Again :(");
+    }
+    else if (guessNum > 0)
+    {
+        Console.WriteLine("You're guess is too high. Try Again :(");
+    }
+    else
+    {
+        Console.WriteLine("You're Right! :)");
+        break;
+    }
+}
+while (favNum == 0);
+
+
+
+
